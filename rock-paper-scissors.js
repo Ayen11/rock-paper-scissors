80aa030640411a151780d9220258ed54cc4a2fe5
Ayen11 <einneuman@gmail.com>
2021-08-03 16:32:47 +0300

    Finished the game, weirdly this is the first video game I've finished and not the game that consumed 2 years of my life

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,12 @@
 let rps = ['rock','paper','scissors'];
 let computerChoice = 0;
-let playerChoice = 0
+let playerChoice = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-let playerChoiceText = prompt("Please choose: rock, paper or scissors", 'paper');
-playerChoiceText = playerChoiceText.toLowerCase();
+
+
+
 
 
 function computerPlay() {
@@ -37,33 +40,66 @@ function singleRound (playerChoice , computerChoice) {
     }
     else if (computerChoice === 0 && playerChoice === 2) {
         //if the computer chooses rock and player scissors the computer wins
-        console.log("Computer wins! rock beats scissors!")
+        console.log("Computer wins! rock beats scissors!");
+        computerScore = ++computerScore;
     } 
     else if (computerChoice === 1 && playerChoice === 0) {
         //if computer chooses paper and the player rock the computer wins
-        console.log("Computer wins! paper beats rock!")
+        console.log("Computer wins! paper beats rock!");
+        computerScore = ++computerScore;
     }
     else if (computerChoice === 2 && playerChoice === 1) {
         //if computer chooses scissors and the player paper the computer wins
-        console.log("Computer wins! scissors beat paper!")
+        console.log("Computer wins! scissors beat paper!");
+        computerScore = ++computerScore;
     }
     else if (playerChoice === 0 && computerChoice === 2) {
         //if player chooses rock and the computer scissors the player wins
-        console.log("Player wins! rock beats scissors!")
+        console.log("Player wins! rock beats scissors!");
+        playerScore = ++playerScore;
     }
     else if (playerChoice === 1 && computerChoice === 0) {
         //if player chooses paper and the computer rock the player wins
         console.log("Player wins! paper beats rock!")
+        playerScore = ++playerScore;
     }
     else if (playerChoice === 2 && computerChoice === 1) {
         //if player chooses scissors and the computer paper the player wins
         console.log("Player wins! scissors beat paper!")
+        playerScore = ++playerScore;
     }
     else {
-        console.log(playerChoice);
+        console.log("WTF");
     };
 }
-computerPlay();
-console.log(computerChoice)
-askPlayer(playerChoiceText);
-singleRound(playerChoice, computerChoice);
+
+function game() {
+    
+    computerPlay();
+    askPlayer(playerChoiceText);
+    singleRound(playerChoice, computerChoice);
+    console.log("Player score: " + playerScore);
+    console.log("Computer score: " + computerScore);
+};
+
+for (let i =1 ;i <= 5; ++i) {
+    console.log('------------------');
+    playerChoiceText = prompt("Please choose: rock, paper or scissors", 'paper');
+    playerChoiceText = playerChoiceText.toLowerCase();
+    console.log("Round number: " + i);
+    game();
+    console.log('------------------');
+};
+
+let message = "aahh im not changed!";
+
+if (playerScore < computerScore) {
+    message = "computer wins!";
+}
+else if (playerScore > computerScore) {
+    message = "player wins!";
+}
+else {
+    message = "I hate edge cases, It's a tie >:-(";
+}
+console.log(message);
